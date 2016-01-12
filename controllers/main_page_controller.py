6d5d28633cbec9d47ef1d53f163dfbabc9bab3ac
Ethan Welsh <welshej@gmail.com>
2016-01-12 14:47:35 -0500

    fix paths to point to correct path

--- controllers/main_page_controller.py
+++ controllers/main_page_controller.py
@@ -1,11 +1,18 @@
 import os
+
 import webapp2
 from google.appengine.ext.webapp import template
 
+
 def render_template(handler, template_name, template_values):
-    path = os.path.join(os.path.dirname(__file__), 'templates/' + template_name)
+    path = os.path.join(os.path.dirname(__file__), '..', 'templates', template_name)
+
+    print('\n' * 10)
+    print(path)
+
     html = template.render(path, template_values)
     handler.response.out.write(html)
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
