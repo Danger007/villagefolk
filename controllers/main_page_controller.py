fb5b4bbb85037d16785924327cb8a94747c121e5
Ethan Welsh <welshej@gmail.com>
2016-01-12 14:47:52 -0500

    remove debug print statement

--- controllers/main_page_controller.py
+++ controllers/main_page_controller.py
@@ -7,9 +7,6 @@ from google.appengine.ext.webapp import template
 def render_template(handler, template_name, template_values):
     path = os.path.join(os.path.dirname(__file__), '..', 'templates', template_name)
 
-    print('\n' * 10)
-    print(path)
-
     html = template.render(path, template_values)
     handler.response.out.write(html)
 
